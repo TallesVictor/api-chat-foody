ec54c2ea18b1e5b35f6c76ea53b6f46de1579d74
Talles Victor <tallesvictor2012@gmail.com>
2020-09-30 22:27:16 -0300

    make it better

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Route::middleware('auth:api')->get('/user/1', 'CardapioController@teste');
-Route::get('/items/{id}', 'CardapioController@teste');
+Route::any('/items/{id}', 'CardapioController@teste');
