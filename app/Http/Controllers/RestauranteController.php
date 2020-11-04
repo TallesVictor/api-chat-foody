b90f431a50ee0697df7538d116d2205d0d19ac71
LAPTOP-NF4BHTUU\talle <tallesvictor2012@gmail.com>
2020-11-03 22:15:01 -0300

    no message

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -7,45 +7,53 @@ use Illuminate\Http\Request;
 
 class RestauranteController extends Controller
 {
-    public function insert(Request $request){
+    public function insert(Request $request)
+    {
         $restaurante = new Restaurante();
         $restaurante = $restaurante->insert($request);
+        if (!$restaurante) {
+            return response('Restaurante não encontrado', 404);
+        }
+        return response()->json($restaurante);
     }
 
-    public function search($parametro){
+    public function search($parametro)
+    {
         $restaurante = new Restaurante();
         $restaurante = $restaurante->search($parametro);
-        if(!$restaurante){
+        if (!$restaurante) {
             return response('Restaurante não encontrado', 404);
         }
-        return json_encode($restaurante);
+        return response()->json($restaurante);
     }
 
-    public function list(){
+    public function list()
+    {
         $restaurante = new Restaurante();
         $restaurante = $restaurante->list();
-        if(!$restaurante){
+        if (!$restaurante) {
             return response('Restaurante não encontrado', 404);
         }
-        return json_encode($restaurante);
+        return response()->json($restaurante);
     }
 
-    public function alterar(Request $request){
+    public function alterar(Request $request)
+    {
         $restaurante = new Restaurante();
         $restaurante = $restaurante->alterar($request);
-        if(!$restaurante){
+        if (!$restaurante) {
             return response('Restaurante não encontrado', 404);
         }
-        return json_encode($restaurante);
+        return response()->json($restaurante);
     }
 
-    public function apagar($codigo){
+    public function apagar($codigo)
+    {
         $restaurante = new Restaurante();
         $restaurante = $restaurante->apagar($codigo);
-        if(!$restaurante){
+        if (!$restaurante) {
             return response('Restaurante não encontrado', 404);
         }
-        return json_encode($restaurante);
+        return response()->json($restaurante);
     }
-
 }
--- app/models/Restaurante.php
+++ app/models/Restaurante.php
@@ -19,35 +19,39 @@ class Restaurante extends Model
 
     public function insert(Request $request)
     {
+        $select = "SELECT * FROM restaurante WHERE cnpj = '$request->cnpj'";
+        $select = DB::select($select);
+        if (!$select) {
+            $proprietario = $request->proprietario;
+            $razao_social = $request->razao_social;
+            $cnpj = preg_replace('/[^0-9]/', '', $request->cnpj);
+            $telefone = $request->telefone;
+            $cep = preg_replace('/[^0-9]/', '', $request->cep);
+            $estado = $request->estado;
+            $cidade = $request->cidade;
+            $rua = $request->rua;
+            $numero = $request->numero;
+            $complemento = $request->complemento;
+            $descricao = $request->descricao;
+            $usuario = $request->user;
 
-        $proprietario = $request->proprietario;
-        $razao_social = $request->razao_social;
-        $cnpj = preg_replace('/[^0-9]/', '', $request->cnpj);
-        $telefone = $request->telefone;
-        $cep = preg_replace('/[^0-9]/', '', $request->cep);
-        $estado = $request->estado;
-        $cidade = $request->cidade;
-        $rua = $request->rua;
-        $numero = $request->numero;
-        $complemento = $request->complemento;
-        $descricao = $request->descricao;
-        $usuario = $request->user;
-
-        $insert = "INSERT INTO restaurante
-         (proprietario, razao_social, cnpj, telefone, cep, estado, cidade, rua, numero, complemento, descricao, user_id, created_at, updated_at)
-         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW() )";
-        return DB::insert($insert, [$proprietario, $razao_social, $cnpj, $telefone, $cep, $estado, $cidade, $rua, $numero, $complemento, $descricao, $usuario]);
+            $insert = "INSERT INTO restaurante
+                        (proprietario, razao_social, cnpj, telefone, cep, estado, cidade, rua, numero, complemento, descricao, user_id, created_at, updated_at)
+                       VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW() )";
+            return DB::insert($insert, [$proprietario, $razao_social, $cnpj, $telefone, $cep, $estado, $cidade, $rua, $numero, $complemento, $descricao, $usuario]);
+        }
+        return null;
     }
 
     public function search($parametro)
     {
-        $select = "SELECT * FROM restaurante WHERE cnpj like '%$parametro%'";
+        $select = "SELECT * FROM restaurante WHERE cnpj like '%$parametro%' LIMIT 1";
         $select = DB::select($select);
         if (!$select) {
-            $select = "SELECT * FROM restaurante WHERE razao_social like '%$parametro%' ";
+            $select = "SELECT * FROM restaurante WHERE razao_social like '%$parametro%' LIMIT 1";
             return  DB::select($select);
         }
-        return $select;
+        return $select[0];
     }
 
     public function list()
