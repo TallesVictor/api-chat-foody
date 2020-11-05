116907f01fb2f901fc2875dbb0aae4d7cca60bb1
LAPTOP-NF4BHTUU\talle <tallesvictor2012@gmail.com>
2020-11-05 20:16:27 -0300

    no message

--- app/models/Restaurante.php
+++ app/models/Restaurante.php
@@ -98,10 +98,10 @@ class Restaurante extends Model
         $idRest = DB::table('restaurante')->where('user_id', $codigo)->value('id');
         $cardapio = DB::table('cardapio')->where('restaurante_id', $idRest)->pluck('id');
         var_dump($cardapio);
-        // foreach ($cardapio as $key) {
-        //     echo $key->id;
-        //     // Prato::where('cardapio_id', $key->id)->delete();
-        // }
+        foreach ($cardapio as $key => $id) {
+            echo $id;
+            // Prato::where('cardapio_id', $key->id)->delete();
+        }
         // $restaurante->delete();
         // $cardapio->delete();
         // User::where('id', $codigo)->delete();
