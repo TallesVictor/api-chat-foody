e7fa333868389e44abe097667b42b53950d639dc
LAPTOP-NF4BHTUU\talle <tallesvictor2012@gmail.com>
2020-11-05 17:51:02 -0300

    no message

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group([
     ], function () {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
-        Route::get('edit', 'AuthController@edit');
+        Route::post('edit', 'AuthController@edit');
     });
 });
 
