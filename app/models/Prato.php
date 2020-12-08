1dccd240da0da775d00c68839cdb0ae100c2c94c
LAPTOP-NF4BHTUU\talle <tallesvictor2012@gmail.com>
2020-12-08 14:00:11 -0300

    no message

--- app/models/Prato.php
+++ app/models/Prato.php
@@ -73,7 +73,7 @@ class Prato extends Model
         $prato = Prato::find($request->id);
 
         if (!$request->url) {
-            $request->url = $prato->url;
+            $request->urls = $prato->url;
             return response()
             ->json($request->all());
         }
